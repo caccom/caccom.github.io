0e60683d28f6cfe6cbf4f38fb968f77094eb6020
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-02-14 10:19:46 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['assets/js/index.min.js'],
+        dest: 'assets/js/index.min.js',
+      },
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'assets/js/index.min.js': ['assets/js/index.js']
+        }
+      }
+    },
+
+    sass: {
+      build: {
+          options: {
+            style: 'compressed' //Seems to be the same as minified
+          },
+        files: {
+          'assets/css/style.css': 'assets/css/style.sass',
+        }
+      }
+    },
+
+    autoprefixer: {
+      dist: {
+        options: {
+          map: true
+        },
+        files: {
+          'assets/css/style.css': 'assets/css/style.css',
+        }
+      }
+    },
+
+    pug: {
+      compile: {
+        files: {
+          'index.html':'index.pug'
+        }
+      },
+    },
+
+    watch: {
+      pug: {
+        files: ['index.pug', 'includes/*.pug'],
+        tasks: ['pug'],
+      },
+      sass: {
+        files: ['assets/css/style.sass', 'assets/css/_partials/*.sass'],
+        tasks: ['sass'],
+      },
+      autoprefixer:{
+        files: ['assets/css/style.css'],
+        tasks: ['autoprefixer'],
+      },
+      uglify: {
+        files: ['assets/js/index.js'],
+        tasks: ['uglify'],
+      },
+      concat: {
+        files: ['assets/js/index.min.js'],
+        tasks: ['concat'],
+      },
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            'assets/css/style.css',
+            'assets/js/index.min.js',
+            'index.html'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,82 @@
+<p align="center">
+  <img src="http://i.imgbox.com/Di0zXjiK.png" width="100">
+</p>
+
+# CACCOM Webste
+
+[Access the website](http://caccom.github.io/caccom)
+
+This project uses:
+- [Pug](https://pugjs.org/)
+- [Sass](http://sass-lang.com/)
+- [Grunt](https://gruntjs.com/)
+- [Browsersync](https://www.browsersync.io/)
+
+## Getting Started
+
+### Installation
+
+First of all, install the dependencies to run this boilerplate.
+
+- [NodeJS](http://nodejs.org/)
+- [Grunt](https://gruntjs.com/)
+
+```sh
+# Clone this repository
+git clone git@github.com:caccom/caccom.github.io.git
+cd caccom.github.io
+
+# install dependencies
+npm install
+
+```
+
+After that, you should be good to go :)
+
+
+### Folders/Files Structure
+
+```sh
+├── assets/
+│   ├── css/
+│   │   └── *.sass
+│   │   └── *.css
+│   ├── img
+│   │   └── *.png
+│   │   └── *.jpg
+│   │   └── *.svg
+│   │   └── favicon.ico
+│   ├── js/
+│   │   └── *.js
+├── includes/
+│   ├── _*.pug
+├── Gruntfile.js
+├── index.html
+├── index.pug
+├── LICENSE.md
+├── package.json
+├── README.md
+```
+These structure will change during project development.
+
+
+### Code Standarts
+
+This project uses [LFeh's Coding Style Guide](https://github.com/LFeh/coding-style)
+
+
+### Tasks
+
+- `grunt concat`: concat `*.js` into `index.min.js`
+- `grunt uglify`: uglifies `index.min.js` into `index.min.js`
+- `grunt sass`: build `*.sass` into `style.css`
+- `grunt autoprefixer`: prefixes `*.css` into style.css
+- `grunt watch`: watch files for changes
+
+- `grunt compile`: compile all `.sass`, `.pug` files, autoprefixes `.css`, concat/uglify `.js`, and optimize images.
+- `grunt`: run watch and browsersync tasks - local server.
+
+
+## License
+
+MIT License
